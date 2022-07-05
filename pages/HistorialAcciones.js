1424dd5b52f91a4eeb6000530632410701c62ebd
Esteban Lillo <estebanlilloc@gmail.com>
2022-07-05 18:39:40 -0400

    Se bloquean paginas de consulta expediente e historial de acciones

--- pages/HistorialAcciones.js
+++ pages/HistorialAcciones.js
@@ -1,12 +1,14 @@
 import { Typography, Box } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import ContentLoader, { BulletList } from "react-content-loader";
 import { DataGrid } from "@mui/x-data-grid";
 import styles from "@/styles/Home.module.css";
 import ButtonConsulta from "@/components/ButtonConsulta";
 import InputTextBusqueda from "@/components/InputTextBusqueda";
+import { useRouter } from "next/router";
 
 export default function Consulta() {
+  const [habilitado, setHabilitado] = React.useState(false);
   const [usuario, setUsuario] = React.useState("");
   const [data, setData] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
@@ -43,35 +45,43 @@ export default function Consulta() {
         setIsLoading(false);
       });
   };
+  if (!habilitado) {
+    const router = useRouter();
+    useEffect(() => {
+      router.push("/not-found");
+    }, []);
 
-  return (
-    <>
-      <h1 className={styles.title}>
-        Servicio de consulta historial acciones de usuario
-      </h1>
+    return <></>;
+  } else {
+    return (
+      <>
+        <h1 className={styles.title}>
+          Servicio de consulta historial acciones de usuario
+        </h1>
 
-      <Typography variant="h6" component="h6">
-        Escribe un nombre de usuario (opcional)
-      </Typography>
+        <Typography variant="h6" component="h6">
+          Escribe un nombre de usuario (opcional)
+        </Typography>
 
-      <form onSubmit={handleSubmit}>
-        <Box sx={{ mt: 1, display: "flex", flexDirection: "column" }}>
-          <InputTextBusqueda
-            onChangeHandler={onChangeHandler}
-            value={usuario}
-            id="usuario"
-            label="Nombre de usuario"
-          />
-          <ButtonConsulta isLoading={isLoading} />
-        </Box>
-      </form>
-      {isLoading && <BulletList />}
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ mt: 1, display: "flex", flexDirection: "column" }}>
+            <InputTextBusqueda
+              onChangeHandler={onChangeHandler}
+              value={usuario}
+              id="usuario"
+              label="Nombre de usuario"
+            />
+            <ButtonConsulta isLoading={isLoading} />
+          </Box>
+        </form>
+        {isLoading && <BulletList />}
 
-      {data && data.length > 0 && (
-        <div style={{ height: 700, width: "100%" }}>
-          <DataGrid columns={columns} rows={data} pageSize={50} />
-        </div>
-      )}
-    </>
-  );
+        {data && data.length > 0 && (
+          <div style={{ height: 700, width: "100%" }}>
+            <DataGrid columns={columns} rows={data} pageSize={50} />
+          </div>
+        )}
+      </>
+    );
+  }
 }
